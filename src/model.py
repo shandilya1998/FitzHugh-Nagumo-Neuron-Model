d45c8af7b1b9dc4cc5497a8d3a84e2211ad632f8
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-10-19 18:46:41 +0530

    first commit

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+class FNNeuron:
+    def __init__(self, dt, niter):
+        self.dt = dt
+        self.niter = niter
+
+    def reset(self):
+        return
+
+    def plot(self):
+        return 
+    
+    def __call__(self):
+        return 0
+
+    
+        
