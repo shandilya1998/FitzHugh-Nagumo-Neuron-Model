433c13be8dfb0cba1adc640ee3ec9ab8dcdb47ca
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-10-19 20:43:07 +0530

    commit experiment 2 part 1

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 - The phase plots were plotted for values of b ranging from 0 to 1 and I\_ext ranging from 0 to 3.
 - As the values of I\_ext increased for a fixed value of b, the neuron varied from a a fixed point to a limit cycle and then again a fixed point behaviour. 
 - As the value of b increased, the value of I\_ext for which a limit cycle bevaviour was observed also increases
+
+##Experiment 2
+# Part 1
+Phase plane for I\_ext=0 were plotted for the following values of b
+- 0
+- 0.4
+- 0.8
+- -0.5
+
+The following is the plot for b = 0
+![case1](../images/exp2/case_1a_phase_plot_num_iter_100_b_0.0000_dt_0.0010_I_ext_0.0000_niter_100000.png)
+
+The following is the plot for b = 0.4
+![case2](../images/exp2/case_1a_phase_plot_num_iter_100_b_0.4000_dt_0.0010_I_ext_0.0000_niter_100000.png)
+
+The following is the plot for b = 0.8 
+![case3](../images/exp2/case_1a_phase_plot_num_iter_100_b_0.8000_dt_0.0010_I_ext_0.0000_niter_100000.png)
+
+The following is the plot for b = -0.5
+![case4](../images/exp2/case_1a_phase_plot_num_iter_50_b_-0.5000_dt_0.0010_I_ext_0.0000_niter_100000.png)
+
+
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from src.model import FNNeuron
 import os
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def exp1():
     exp_plot_dir = 'images/exp1'
@@ -36,4 +37,41 @@ def exp1():
             fn()
             fn.plot(os.path.join(exp_plot_dir, image_name))
             fn.reset()
-exp1()
+
+def exp2():
+    exp_plot_dir = 'images/exp2'
+    dt = 0.001
+    niter = int(10e4)
+    b = -0.5
+    I_ext = 0 
+    fn = FNNeuron(
+        dt, 
+        niter   
+    )
+    num_exp = 50
+    v = np.random.normal(0, 1, num_exp)
+    w = np.random.normal(0, 1, num_exp)
+    fig, axes = plt.subplots(1, 1,figsize =  (5, 5))
+    for i in tqdm(range(num_exp)):
+        fn.set_b(b)
+        fn.set_v(v[i])
+        fn.set_w(w[i])
+        fn.set_I_ext(I_ext)
+        fn()
+        axes.plot(fn.v_hist, fn.w_hist)
+        axes.set_xlabel('voltage')
+        axes.set_ylabel('recovery variable')
+        axes.set_title('phase plot')
+        fn.reset()
+    if not os.path.exists(exp_plot_dir):
+        os.mkdir(exp_plot_dir)
+    image_name = 'case_1a_phase_plot_num_iter_{num}_b_{val_b:.4f}_dt_{val_dt:.4f}_I_ext_{val_I:.4f}_niter_{n}.png'.format(
+        num = num_exp,
+        val_b = b,
+        val_dt = dt, 
+        val_I = I_ext,
+        n = niter
+    )
+    fig.savefig(os.path.join(exp_plot_dir, image_name))
+
+exp2()
